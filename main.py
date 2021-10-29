6ec5ac5cdd6d2648362f5ab62768218e73dae9b5
rakhmatov93khondamir <khondamir.rakhmatov@gmail.com>
2021-10-29 20:12:16 +0500

    some fixes

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ import turtle
 screen = turtle.Screen()
 screen.title("The Hirst Painting")
 
-
 color_list = [(131, 166, 205), (222, 148, 106), (31, 42, 61), (199, 134, 147), (165, 59, 48), (140, 184, 162), (39, 105, 157), (238, 212, 89), (152, 58, 66), (217, 81, 70), (169, 29, 33), (236, 165, 156), (50, 112, 90), (35, 61, 55), (17, 97, 71), (156, 33, 30), (231, 160, 165), (53, 44, 49), (170, 188, 221), (57, 51, 48), (189, 100, 110), (31, 60, 109), (103, 127, 161), (34, 151, 209), (174, 200, 188), (65, 66, 56)]
 
 x_initial = -200
@@ -25,6 +24,8 @@ y_initial = -200
 turtle.colormode(255)
 timmy = turtle.Turtle()
 timmy.penup()
+timmy.speed(0)
+timmy.hideturtle()
 timmy.goto(x_initial, y_initial)
 
 # print(timmy.xcor())
